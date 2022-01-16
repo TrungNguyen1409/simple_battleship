434e2a3c6e3d08381767820febd90f8e2d03c1df
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-01-16 12:52:45 +0100

    needed logic

--- battleship.js
+++ battleship.js
@@ -10,5 +10,17 @@ var isSunk = false;
 
 while(isSunk == false){
     guess = prompt("Ready, aim, fire! (enter a number from 0-6): ");
-    
+    if(guess<0 || guess >6){
+        alert("Please enter a valid cell number!");
+    }else{
+        guesses++;
+
+        if(guess == location1 || guess == location2 || guess == location3){
+            hits++;
+        }
+        if(hits == 3){
+            isSunk= true;
+            alert("You sank my battleship!");
+        }
+    }
 }
