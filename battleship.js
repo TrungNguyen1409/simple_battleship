0782c45354dd1b2b9e991b9cd348e4b148ae333a
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-01-16 12:46:21 +0100

    declare var and implement loop

---
+++ battleship.js
@@ -0,0 +1,14 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+
+while(isSunk == false){
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6): ");
+    
+}
