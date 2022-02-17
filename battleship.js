4b33d55521912ae17d9049b486f8a4b9809fd362
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-02-17 19:40:15 +0100

    adding function sum(a,b)

--- battleship.js
+++ battleship.js
@@ -33,3 +33,7 @@ while(isSunk == false){
 
 var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means you accruracy was " + (3/guesses)*100+"%" ;
 alert(stats);
+
+function sum(a,b){
+    return a+b;
+}
